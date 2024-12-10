09a90128baeade4f61b901a9a1e16cc229a731ba
0day <nomadify@proton.me>
2024-12-11 00:10:51 +0300

    chore: move getLikes, getDislikes to trait

--- src/Traits/WithVote.php
+++ src/Traits/WithVote.php
@@ -82,4 +82,22 @@ trait WithVote
     {
         return $this->morphMany(getVoteClass(), getMorphName());
     }
+
+    public function getLikes(bool $fromRelation = false): int
+    {
+        if ($fromRelation || !$this->hasAttribute(getLikesColumnName())) {
+            return $this->votes()->where('type', '=', Reaction::LIKE)->count();
+        }
+
+        return $this->{getLikesColumnName()};
+    }
+
+    public function getDislikes(bool $fromRelation = false): int
+    {
+        if (!$fromRelation || !$this->hasAttribute(getDislikesColumnName())) {
+            return $this->votes()->where('type', '=', Reaction::DISLIKE)->count();
+        }
+
+        return $this->{getDislikesColumnName()};
+    }
 }
--- tests/Models/Post.php
+++ tests/Models/Post.php
@@ -17,22 +17,4 @@ class Post extends Model implements Votable
 
     protected $table = 'posts';
     protected $guarded = [];
-
-    public function getLikes(bool $fromRelation = false): int
-    {
-        if ($fromRelation || !$this->hasAttribute(getLikesColumnName())) {
-            return $this->votes()->where('type', '=', Reaction::LIKE)->count();
-        }
-
-        return $this->{getLikesColumnName()};
-    }
-
-    public function getDislikes(bool $fromRelation = false): int
-    {
-        if (!$fromRelation || !$this->hasAttribute(getDislikesColumnName())) {
-            return $this->votes()->where('type', '=', Reaction::DISLIKE)->count();
-        }
-
-        return $this->{getDislikesColumnName()};
-    }
 }
